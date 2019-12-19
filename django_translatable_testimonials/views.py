1cbdacb316880605d875b024cafb18f267025f62
BringTheChill <vflorinrobert@gmail.com>
2019-12-19 15:24:18 +0200

    Moved from hvad to parler. Removed unused template tags.

--- django_translatable_testimonials/views.py
+++ django_translatable_testimonials/views.py
@@ -1,12 +1,14 @@
 from django.views.generic import ListView
-from hvad.utils import get_translation_aware_manager
+# from hvad.utils import get_translation_aware_manager
+from parler.views import TranslatableSlugMixin
+
 from .models import Testimonial
 
 
-class TestimonialsActiveList(ListView):
+class TestimonialsActiveList(TranslatableSlugMixin, ListView):
     model = Testimonial
-    manager = get_translation_aware_manager(Testimonial)
-    queryset = manager.get_queryset().filter(active=True)
+    # manager = get_translation_aware_manager(Testimonial)
+    queryset = Testimonial.objects.get_queryset().filter(active=True)
 
 
 
